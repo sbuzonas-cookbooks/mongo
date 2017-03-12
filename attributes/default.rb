05a4b0af9d6e65bec9d5c70aecd2c34636406a66
Steve Buzonas <steve@fancyguy.com>
2017-03-12 18:55:39 -0400

    bump default mongo version

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,4 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['mongo']['version'] = '3.2.11'
+default['mongo']['version'] = '3.4.2'
