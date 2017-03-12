de7fcb9f14ab29ca20513f8d972f37d037430653
Steve Buzonas <steve@fancyguy.com>
2017-03-11 23:05:33 -0500

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'mongo'
+maintainer 'Steve Buzonas'
+maintainer_email 'steve@fancyguy.com'
+license 'apachev2'
+description 'Installs/Configures mongo'
+long_description 'Installs/Configures mongo'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/mongo/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/mongo' if respond_to?(:source_url)
